d24f7c18c5b3f54da1aec723399ded68ed78b212
zhuoran <liuzrcc@hotmail.com>
2023-02-01 10:16:46 +0100

    update

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python main.py --poison_type TAP  --exp_path ./experiments/TAP/ --poison_path ./data/TAP/ --jpeg 10
+python main.py --poison_type TAP  --exp_path ./experiments/TAP/ --poison_path ./data/TAP/jpeg10/ --jpeg 10
